eeb2a7cf32f31f9704fe0748b26ad842ebdd2cb4
fish <123213839+JJ-gren@users.noreply.github.com>
2025-04-04 11:23:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ const isParentWindow = !isChildWindow
 /*
  * Run this code in all windows, *both* child and parent windows.
  */
-console.log("cooked fishsigma");
+console.log("cooked fishsigma2");
 init()
 
 /*
